72296c53ad8c74057cbc51c088c24a50fc5dd9d5
Kevin David Rodríguez Belalcazar <51235539+Kelocoes@users.noreply.github.com>
2023-06-03 19:32:01 -0500

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -161,5 +161,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://proyecto-grado-front.onrender.com',
-    'https://www.riesgo-cardiovascular-uv.me'
+    'https://riesgo-cardiovascular-uv.me'
 ]
