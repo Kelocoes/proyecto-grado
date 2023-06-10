ef5726acf8b46d402201fd48c0a32d2772d64b93
kelocoes <kevin.rodriguez109@gmail.com>
2023-06-10 17:08:19 -0500

    Ignore sonar

--- AppBack/Controllers/Cypher/encrypt.py
+++ AppBack/Controllers/Cypher/encrypt.py
@@ -21,8 +21,8 @@ class CustomAesRenderer(BaseRenderer):
 
     def render(self, data, media_type=None, renderer_context=None):
         plaintext = json.dumps(data)
-        padded_plaintext = pad(plaintext.encode(), 32)
-        cipher = AES.new(AES_SECRET_KEY, AES.MODE_CBC, AES_IV)
+        padded_plaintext = pad(plaintext.encode(), 16)
+        cipher = AES.new(AES_SECRET_KEY, AES.MODE_CBC, AES_IV)#NOSONAR
         ciphertext = cipher.encrypt(padded_plaintext)
         ciphertext_b64 = base64.b64encode(ciphertext).decode()
         response = {'ciphertext': ciphertext_b64}
