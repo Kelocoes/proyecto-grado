a3095f1c3d71cf87df397e1ab5db554bec408992
Kevin David Rodríguez Belalcazar <51235539+Kelocoes@users.noreply.github.com>
2023-12-19 14:45:40 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-#poetry install
-pip install --upgrade pip #Update pip version
-pip --version # Check pip version
 pip install -r requirements.txt # Install dependencies using requirements.txt
 
 python3 manage.py collectstatic --no-input # Collect all necesary static files
